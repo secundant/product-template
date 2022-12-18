33a43ff63841327c3fd6533d577f8e0c330132a4
Dmitry Remezov <mysecundant@gmail.com>
2022-12-18 13:32:57 +0300

    ci: fix husky, lint-staged and eslint config in docs

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,6 +1,7 @@
 const { presets, configure } = require('eslint-kit');
 
 module.exports = configure({
+  root: __dirname,
   presets: [presets.node({}), presets.imports(), presets.typescript({}), presets.prettier({})],
   extend: {
     rules: {
