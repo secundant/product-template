78ad6cfe215c9958d48259e2504b892492bb7c24
Dmitry Remezov <mysecundant@gmail.com>
2022-12-21 21:39:19 +0300

    feat(ui): create "ui" library

---
+++ libs/ui/src/molecules/button/button.tsx
@@ -0,0 +1,60 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import { ForwardedRef, forwardRef, ReactNode } from 'react';
+
+export interface ButtonProps extends VariantProps<typeof buttonTheme> {
+  className?: string;
+  children?: ReactNode;
+}
+
+export const Button = forwardRef(
+  (
+    { className, size, color, appearance, ...props }: ButtonProps,
+    ref: ForwardedRef<HTMLButtonElement>
+  ) => (
+    <button
+      ref={ref}
+      className={buttonTheme({
+        className,
+        appearance,
+        color,
+        size
+      })}
+      {...props}
+    />
+  )
+);
+
+export const buttonTheme = cva(['flex items-center overflow-hidden'], {
+  variants: {
+    size: {
+      sm: 'p-2',
+      md: 'p-3',
+      lg: 'p-4'
+    },
+    color: {
+      primary: 'border-blue-500',
+      success: 'border-green-500'
+    },
+    appearance: {
+      filled: 'text-white',
+      outlined: 'border rounded-sm'
+    }
+  },
+  defaultVariants: {
+    size: 'sm',
+    color: 'primary',
+    appearance: 'filled'
+  },
+  compoundVariants: [
+    {
+      color: 'primary',
+      appearance: 'filled',
+      className: 'bg-blue-500'
+    },
+    {
+      color: 'success',
+      appearance: 'filled',
+      className: 'bg-green-500'
+    }
+  ]
+});
