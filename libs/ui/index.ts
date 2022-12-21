998bbdf8877ccf0f13b53ad70fc4d901d2a9c637
Dmitry Remezov <mysecundant@gmail.com>
2022-12-21 21:39:19 +0300

    feat(ui): create "ui" library

---
+++ libs/ui/index.ts
@@ -0,0 +1,4 @@
+import './src/index.css';
+
+export type { ButtonProps } from './src/molecules';
+export { Button, buttonTheme } from './src/molecules';
---
+++ libs/ui/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.tsx', './**/*.mdx'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
