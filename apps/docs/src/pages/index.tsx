749e12c6198b4666e7a6b7bc3f46927cea28ac84
Dmitry Remezov <mysecundant@gmail.com>
2022-12-21 21:41:03 +0300

    feat(docs): drop default page and components, start customization

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,26 +1,7 @@
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import React from 'react';
-import HomepageFeatures from '../components/HomepageFeatures';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={styles.heroBanner}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import { HeroCardLink } from '../shared/ui';
 
 // eslint-disable-next-line import/no-default-export
 export default function Home() {
@@ -30,10 +11,21 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <section className="container mx-auto mb-16 py-32">
+        <h1 className="mb-16 text-6xl lg:text-8xl">My organization</h1>
+        <h2 className="text-4xl lg:text-6xl">Shared knowledge base</h2>
+      </section>
+      <div className="container mb-16 grid grid-cols-1 gap-8 lg:grid-cols-3">
+        <HeroCardLink to="/docs/intro" label="Getting started">
+          You're newcomer? Welcome!
+        </HeroCardLink>
+        <HeroCardLink to="/docs/apps" label="Products">
+          Here you can found documentation for your current project
+        </HeroCardLink>
+        <HeroCardLink to="/docs/libraries" label="Libraries">
+          Explore our primary libraries
+        </HeroCardLink>
+      </div>
     </Layout>
   );
 }
