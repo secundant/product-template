14ddccf750381f4e139850cbca6b33b97f996598
Dmitry Remezov <mysecundant@gmail.com>
2022-12-26 21:56:26 +0300

    feat(docs): add "package manager" section, start "monorepo" page

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -11,18 +11,31 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <section className="container mx-auto mb-16 py-32">
-        <h1 className="mb-16 text-6xl lg:text-8xl">My organization</h1>
-        <h2 className="text-4xl lg:text-6xl">Shared knowledge base</h2>
+      <section className="container mx-auto grid grid-cols-1 gap-8 py-16 md:grid-cols-2 md:py-32">
+        <div>
+          <h1 className="mb-16 text-6xl lg:text-8xl">My organization</h1>
+          <h2 className="text-4xl lg:text-6xl">Shared knowledge base</h2>
+        </div>
+        <div className="shrink grow-0 self-center justify-self-center">
+          <HeroCardLink
+            to="/docs/product-template/intro"
+            icon="💁🏽‍♀️"
+            label="About template"
+            className="max-w-xl shadow-2xl"
+          >
+            It's designed as full-featured starter for medium-size products or even open source
+            libraries repository
+          </HeroCardLink>
+        </div>
       </section>
       <div className="container mb-16 grid grid-cols-1 gap-8 lg:grid-cols-3">
-        <HeroCardLink to="/docs/intro" label="Getting started">
-          You're newcomer? Welcome!
+        <HeroCardLink to="/docs/intro" icon="👐" label="%Getting started% place">
+          Something like "You're newcomer? Welcome! Start your journey from here"
         </HeroCardLink>
-        <HeroCardLink to="/docs/product-template/intro" label="Projects">
-          Here you can found documentation for your current project
+        <HeroCardLink to="/docs/product-template/intro" icon="️🪑" label="%Our projects% place">
+          %Link to company's products overview%
         </HeroCardLink>
-        <HeroCardLink to="/docs/libraries" label="Libraries">
+        <HeroCardLink to="/docs/libraries" icon="📚" label="Libraries">
           Explore our primary libraries
         </HeroCardLink>
       </div>
--- apps/docs/src/shared/ui/hero-card-link.tsx
+++ apps/docs/src/shared/ui/hero-card-link.tsx
@@ -4,22 +4,30 @@ import React, { ReactNode } from 'react';
 
 export interface HeroCardLinkProps {
   to: string;
+  icon: ReactNode;
   label: ReactNode;
   children: ReactNode;
+  className?: string;
 }
 
-export function HeroCardLink({ to, label, children }: HeroCardLinkProps) {
+export function HeroCardLink({ to, icon, label, children, className }: HeroCardLinkProps) {
   return (
     <Link
       to={to}
       className={clsx(
-        'block rounded-lg border bg-stone-600 p-4 text-white transition md:p-8',
-        'hover:bg-stone-700 hover:text-white hover:shadow-lg',
-        'dark:bg-stone-300 dark:text-black dark:hover:bg-stone-400'
+        'block rounded-xl border bg-zinc-200 p-4 shadow-sm transition md:p-8',
+        'hover:scale-105 hover:bg-zinc-300 hover:no-underline hover:shadow-2xl active:scale-100',
+        'dark:bg-zinc-800 dark:hover:bg-zinc-700',
+        className
       )}
     >
-      <h5 className="mb-4 text-lg">{label}</h5>
-      {children}
+      <h5 className="mb-4 text-xl">{label}</h5>
+      <div className="flex gap-4">
+        <div className="flex h-16 w-16 shrink-0 items-center justify-center rounded-xl bg-stone-400 text-2xl dark:bg-stone-900">
+          {icon}
+        </div>
+        {children}
+      </div>
     </Link>
   );
 }
